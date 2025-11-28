96c26db73eb4a0bd3e400787cf0341a98f3a00a5
micaelenn <mirandamicaelen@gmail.com>
2025-11-28 12:55:27 -0300

    feat: general improvements: - add color highlights - add info to ensure sudo mode run - ensure website name is properly formatted (lowercase and sanitize)

--- README.md
+++ README.md
@@ -4,9 +4,8 @@ A collection of Bash scripts designed to streamline local setups on Linux
 
 ## wpsetup.sh
 
-This automation streamlines repetitive WordPress setup tasks by downloading the latest WordPress version, configuring Apache virtual hosts, and creating a MySQL database with minimal manual input. To enable automatic database creation and import, you must create a ``databases`` directory and add an SQL file named after the website.
+This automation streamlines repetitive WordPress setup tasks by downloading the latest WordPress version, configuring Apache virtual hosts, and creating a MySQL database with minimal manual input. To enable automatic database import, you must create a ``databases`` directory and add an SQL file named after the website.
 
 ### :hammer: Next Steps
 
-- Ensure the project name is always formatted in lowercase
 - Automatically generate and configure .htaccess
--- wpsetup.sh
+++ wpsetup.sh
@@ -1,28 +1,67 @@
 #!/bin/bash
 
-# initial configs
-read -p "Enter the website name (ex: amazingproject): " websitename
-read -p "Enter the GitHub URL: " githuburl
-read -p "Enter the MySQL root username : " mysql_user
+# colors
+RED='\e[31m'
+BLU='\e[34m'
+GRN='\e[32m'
+DEF='\e[0m'
 
-clear
-
-## WORDPRESS CONFIGS
-cd /var/www/ || exit
-
-# clone the repository if provided
-if [ -n "$githuburl" ]; then
-  git clone "$githuburl" "$websitename" || { echo "Something went wrong. Please, check the GitHub URL and try again."; }
-else
-  echo "No GitHub URL provided. Procceding with a new WordPress installation."
+if [ "$EUID" -ne 0 ]; then
+  echo -e "${RED}Error:${DEF} This script must be executed with elevated privileges (sudo)."
+  echo -e "${BLU}Usage${DEF}: sudo bash ./wpsetup.sh"
+  exit 1
 fi
+
+echo -e "${GRN}✔ Running in sudo mode...${DEF}"
+
+# website name
+while true; do
+  read -p "Enter the website name (max 20 chars): " websitename
+
+  # lowercase and sanitize
+  websitename=$(echo "$websitename" | tr '[:upper:]' '[:lower:]')
+  cleanedname=$(echo "$websitename" | sed 's/[^a-z0-9]//g')
+
+  # empty name
+  if [ -z "$cleanedname" ]; then
+    echo -e "${RED}Error:${DEF} The project name cannot be empty."
+    continue
+  fi
+
+  # name too long
+  if [ ${#cleanedname} -gt 20 ]; then
+    echo -e "${RED}Error:${DEF} The project name is too long (max 20 chars)."
+    continue
+  fi
+  websitename="$cleanedname"
+  break
+done
+
+# clone repository
+cd /var/www/ || exit
+mkdir "$websitename"
+
+while true; do
+  read -p "Please enter the repository URL (or leave empty for a new WordPress installation): " repositoryurl
+
+  if [ -z "$repositoryurl" ]; then
+    echo -e "${BLU}Info:${DEF} No repository URL provided. Proceeding with a fresh WordPress installation."
+    break
+  fi
+
+  git clone "$repositoryurl" "$websitename"
+  if [ $? -eq 0 ]; then
+    echo -e "${GRN}Repository cloned successfully!${DEF}"
+    break
+  else
+    echo -e "${RED}Error:${DEF} Invalid repository URL."
+  fi
+done
 
 cd "$websitename"
 
-clear
-
 # download and extract latest WordPress
-echo "Downloading latest WordPress version"
+echo -e "${BLU}Downloading the latest WordPress version...${DEF}"
 wget https://wordpress.org/latest.tar.gz -O /tmp/latest.tar.gz
 tar -xzf /tmp/latest.tar.gz -C /tmp/
 rm /tmp/latest.tar.gz
@@ -39,33 +78,32 @@ sed -i "s/database_name_here/${websitename}/" wp-config.php
 rm -rf /tmp/wordpress
 
 clear
+echo -e "${GRN}WordPress dependencies successfully installed! ✔${DEF}"
 
-## MYSQL CONFIGS
-echo "Setting up MySQL database"
-sudo mysql -u "${mysql_user}" -e "CREATE DATABASE ${websitename} DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;"
+## MySQL configs
+echo -e "${BLU}Creating MySQL database...${DEF}"
+mysql -u -e "CREATE DATABASE ${websitename} DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;"
 
 cd ~
 sql_file="databases/${websitename}.sql"
 gz_file="${sql_file}.gz"
-message="Importing SQL file. Please wait, this may take a while..."
+message="${BLU}Importing SQL file. Please wait, this may take a while...${DEF}"
 
 if [ -f "$gz_file" ]; then
-  echo "$message"
-  zcat "${gz_file}" | sudo mysql -u "${mysql_user}" "${websitename}"
+  echo -e "$message"
+  zcat "${gz_file}" | mysql -u "${websitename}"
 elif [ -f "$sql_file" ]; then
-  echo "$message"
-  sudo mysql -u "${mysql_user}" "${websitename}" < "${sql_file}"
+  echo -e "$message"
+  mysql -u "${websitename}" < "${sql_file}"
 else
-  echo "No sql file found."
+  echo -e "${BLU}No SQL file to import. Proceding with empty database...${DEF}"
 fi
 
-clear
-
-## APACHE CONFIGS
-echo "Creating Apache config for $websitename..."
+## Apache configs
+echo -e "${BLU}Creating Apache config for ${websitename}...${DEF}"
 config_path="/etc/apache2/sites-available/${websitename}.conf"
 
-sudo bash -c "cat > '$config_path' <<EOF
+bash -c "cat > '$config_path' <<EOF
 <VirtualHost *:80>
     DocumentRoot /var/www/${websitename}
     ServerName ${websitename}.local
@@ -86,8 +124,9 @@ EOF
 "
 
 # enable site and reload Apache
-sudo a2ensite "${websitename}.conf"
-sudo systemctl reload apache2
+a2ensite "${websitename}.conf"
+systemctl reload apache2
 
 clear
-echo "Done! Your website is available on http://${websitename}.local"
+echo -e "${GRN}Done!${DEF}"
+echo -e "Your website is available on ${BLU}http://${websitename}.local${DEF} ✔"
